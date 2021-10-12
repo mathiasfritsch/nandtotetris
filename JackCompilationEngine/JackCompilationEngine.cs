f6a25b91d911bca8e63ff5a3ca48f2cc6def4f31
mathiasfritsch <mathias.fritsch.hh@gmail.comm>
2021-10-12 08:19:59 +0200

    jack compiler

---
+++ JackCompilationEngine/JackCompilationEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO.Abstractions;
+
+namespace JackCompilationEngine
+{
+    public class Tokenizer
+    {
+        public Tokenizer(FileSystem fileSystem, string sourcePath)
+        {
+        }
+
+        public void WriteXml(string targetPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JackCompiler/Program.cs
@@ -0,0 +1,18 @@
+﻿using JackCompilationEngine;
+using System;
+using System.IO.Abstractions;
+
+namespace JackCompiler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sourcePath = args.Length > 0 ? args[0] : @"C:\\projects\\nand2tetris\\projects\\10\\ArrayTest";
+            var targetPath = args.Length > 1 ? args[1] : @"C:\TEMP\FibonacciElementDebug\FibonacciElement.asm";
+
+            Tokenizer tokenizer = new Tokenizer(new FileSystem(), sourcePath);
+            //tokenizer.WriteXml(targetPath);
+        }
+    }
+}
